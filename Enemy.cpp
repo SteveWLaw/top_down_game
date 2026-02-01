431e1d227f493fbc19c1db6434765b8030d554fe
Steve Law <steve.w.law@gmail.com>
2026-02-01 13:21:14 +0000

    Refactor Enemy class to improve movement logic; add missing include for raymath.h and enhance target direction calculation to allow goblin to chase knight (4.24)

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,5 +1,6 @@
 #include "Enemy.h"
 #include "raylib.h"
+#include "raymath.h"
 
 Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 
@@ -10,9 +11,25 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     run = run_texture;
     width = texture.width / maxFrames;
     height = texture.height;
+    speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime)
 {
+
+
+    // get toTarget direction as a vector
+    Vector2 toTarget{Vector2Subtract(target->getScreenPos(), screenPos)};
+
+    // normalise toTarget
+    toTarget = Vector2Normalize(toTarget);
+
+    // multiply normalised vector by speed and deltaTime to get velocity
+    toTarget = Vector2Scale(toTarget, speed);
+
+    // move enemy toward the target
+    worldPos = Vector2Add(worldPos, toTarget);
+
+    screenPos = Vector2Subtract(worldPos, target->getWorldPos());
     BaseCharacter::tick(deltaTime);
 }
